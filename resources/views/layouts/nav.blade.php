c805688a0b592d301195e76f6b66004a36ae3e88
Daba Clovis <dabaclovis@users.noreply.github.com>
2024-11-15 18:41:25 -0500

    my firstapp

--- resources/views/layouts/nav.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <a href="/home" class="d-inline btn btn-secondary text-white">Dashboard</a>
-    <a href="/articles/create" class="d-inline btn btn-secondary text-white">Add Article</a>
-</div>
